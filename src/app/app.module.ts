b3ee61451c164570a20fd13d1456ca653362ddcb
Rodger Freitas <rodger.freitas@gmail.com>
2019-01-30 17:16:13 -0200

    andamento no front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactComponent } from './contacts/contact/contact.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactService } from './shared/contact.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ContactComponent,
+    ContactListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
-  providers: [],
+  providers: [ContactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/contact.model.ts
@@ -0,0 +1,6 @@
+export class Contact {
+    id: number;
+    nome: string;
+    telefone: string;
+    email: string;
+}
