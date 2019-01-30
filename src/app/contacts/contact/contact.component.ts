b238dabad43a982c39289de964dc6988c0e97e82
Rodger Freitas <rodger.freitas@gmail.com>
2019-01-30 17:50:35 -0200

    andamento no front end

--- src/app/contacts/contact/contact.component.ts
+++ src/app/contacts/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ContactService} from '../../shared/contact.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -8,9 +9,46 @@ import {ContactService} from '../../shared/contact.service';
 })
 export class ContactComponent implements OnInit {
 
-  constructor(private service : ContactService) { }
+  constructor(private service: ContactService) { }
 
   ngOnInit() {
+    this.resetForm();
   }
 
+  resetForm(form?: NgForm) {
+    if (form != null) {
+      form.resetForm();
+    }
+    this.service.formData = {
+      id: null,
+      nome: '',
+      telefone: '',
+      email: ''
+    };
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.value.id == null){
+      this.insertRecord(form);
+    } else {
+      this.updateRecord(form);
+    }
+  }
+
+  insertRecord(form: NgForm) {
+    // this.service.postEmployee(form.value).subscribe(res => {
+    //   this.toastr.success('Inserted successfully', 'EMP. Register');
+    //   this.resetForm(form);
+    //   this.service.refreshList();
+    // });
+  }
+
+  updateRecord(form: NgForm) {
+    // this.service.putEmployee(form.value).subscribe(res => {
+    //   this.toastr.info('Updated successfully', 'EMP. Register');
+    //   this.resetForm(form);
+    //   this.service.refreshList();
+    // });
+
+  }
 }
